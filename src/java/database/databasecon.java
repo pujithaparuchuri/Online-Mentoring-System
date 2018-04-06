0217659560e5dec609e615ce7a9c9c8936f1686a
Krishna <pujitha.paruchuri@gmail.com>
2018-04-06 16:19:30 -0400

    Add files via upload

---
+++ src/java/database/databasecon.java
@@ -0,0 +1,23 @@
+package database;
+import java.sql.*;
+
+public class databasecon
+{
+	static Connection con;
+	public static Connection getconnection()
+	{
+ 		
+ 			
+		try
+		{
+			Class.forName("com.mysql.jdbc.Driver");	
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/OnlineMentoringSystem","root","root");
+		}
+		catch(Exception e)
+		{
+			System.out.println("class error"+e);
+		}
+		return con;
+	}
+	
+}
